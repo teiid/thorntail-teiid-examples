922693e014b01c298bfebe7a3940317805331d8f
kylinsoong <kylinsoong.1214@gmail.com>
2016-09-29 18:29:18 +0800

    add a empty source example

--- empty-source/src/main/java/org/wildfly/swarm/teiid/examples/Main.java
+++ empty-source/src/main/java/org/wildfly/swarm/teiid/examples/Main.java
@@ -2,7 +2,6 @@ package org.wildfly.swarm.teiid.examples;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.wildfly.swarm.Swarm;
-import org.wildfly.swarm.jaxrs.JAXRSArchive;
 import org.wildfly.swarm.teiid.VDBArchive;
 
 public class Main {
@@ -15,10 +14,6 @@ public class Main {
         VDBArchive vdb = ShrinkWrap.create(VDBArchive.class);
         vdb.vdb(Main.class.getClassLoader().getResourceAsStream("empty-vdb.xml"));
         swarm.deploy(vdb);    
-        
-        JAXRSArchive testRs = ShrinkWrap.create(JAXRSArchive.class);
-        testRs.addResource(TestResource.class);
-        swarm.deploy(testRs);
 
     }
 
