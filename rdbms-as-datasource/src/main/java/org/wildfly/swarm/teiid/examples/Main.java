8b7c1612309b73728d5a82dfd7d6dcc4b6eac0b7
kylinsoong <kylinsoong.1214@gmail.com>
2016-09-30 17:51:06 +0800

    add some example

--- rdbms-as-datasource/src/main/java/org/wildfly/swarm/teiid/examples/Main.java
+++ rdbms-as-datasource/src/main/java/org/wildfly/swarm/teiid/examples/Main.java
@@ -1,37 +1,50 @@
 package org.wildfly.swarm.teiid.examples;
 
+import java.io.InputStreamReader;
+import java.sql.Connection;
+
+import org.h2.tools.RunScript;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.datasources.DatasourceArchive;
+import org.wildfly.swarm.teiid.TeiidFraction;
+import org.wildfly.swarm.teiid.VDBArchive;
 
 public class Main {
     
+    private static String URL = "jdbc:h2:file:" + System.getProperty("java.io.tmpdir") + "/test";
+    
     public static void main(String[] args) throws Exception {
         
-        // 1. start container, configure translator
-        Swarm swarm = new Swarm();
+        setupH2();
         
-//        TeiidFraction f = new TeiidFraction();
-//        f.translator("h2", t -> t.module("org.jboss.teiid.translator.jdbc"));   
-//        swarm.fraction(f);
-
+        // 1. start container, configure translator
+        Swarm swarm = new Swarm();        
+        swarm.fraction(new TeiidFraction().translator("h2", t -> t.module("org.jboss.teiid.translator.jdbc")));
         swarm.start();
 
         // 2. deploy data sources/resource adapter
-//        swarm.deploy(Swarm.artifact("com.h2database:h2", "h2"));
-//        DatasourceArchive dsArchive = ShrinkWrap.create(DatasourceArchive.class);
-//        dsArchive.dataSource("accounts-ds", (ds) -> {
-//            ds.connectionUrl("jdbc:h2:mem:accounts;INIT=RUNSCRIPT FROM '" + teiidFiles + "/customer-schema.sql';");
-//            ds.driverName("h2");
-//            ds.userName("sa");
-//            ds.password("sa");
-//        });
-//        swarm.deploy(dsArchive);
+        swarm.deploy(Swarm.artifact("com.h2database:h2", "h2"));
+        DatasourceArchive dsArchive = ShrinkWrap.create(DatasourceArchive.class);
+        dsArchive.dataSource("accounts-ds", (ds) -> {
+            ds.connectionUrl(URL);
+            ds.driverName("h2");
+            ds.userName("sa");
+            ds.password("sa");
+        });
+        swarm.deploy(dsArchive);
 
         // 3. deploy VDB
-//        VDBArchive vdb = ShrinkWrap.create(VDBArchive.class);
-//        vdb.vdb(Main.class.getClassLoader().getResourceAsStream("portfolio-vdb.xml"));
-//        swarm.deploy(vdb);     
+        VDBArchive vdb = ShrinkWrap.create(VDBArchive.class);
+        vdb.vdb(Main.class.getClassLoader().getResourceAsStream("portfolio-vdb.xml"));
+        swarm.deploy(vdb);     
     }
 
+    private static void setupH2() throws Exception {
 
+        Connection conn = JDBCUtils.getDriverConnection("org.h2.Driver", URL, "sa", "sa");
+        RunScript.execute(conn, new InputStreamReader(Main.class.getClassLoader().getResourceAsStream("customer-schema.sql")));
+        JDBCUtils.close(conn);
+    }
     
 }
